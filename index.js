a77072d38dd861a9a2b62f963e99ef6d4b06b265
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-08-15 21:38:57 -0400

    commit tree 2e811ca9283e3f5859deee78bdb830b4e6611a6a

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+
+const formData = {
+    name,
+    email
+};
+
+const configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+ };   
+
+// function submitData(name, email) {
+//     return fetch("http://localhost:3000/users", configObj)
+//     .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(object) {
+//         document.body.innerHTML = object['id']
+//     })
+//     .catch( function ( error ) {
+//         document.body.innerHTML = error.message
+//       } )
+// }
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function(object) {
+        document.body.innerHTML = object['id']
+        })
+        .catch( function ( error ) {
+            document.body.innerHTML = error.message
+          } )
+}
